25f045a497447d19693edb819064e81328dd71a9
hsanjebri <hsan.jebri@esprit.tn>
2025-01-07 12:36:04 +0100

    Update vite.config.js with base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3D-Web-Developer-Portfolio-in-React-JS/', // Set base path for GitHub Pages
   plugins: [react()],
 })
